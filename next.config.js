4f2d5582f236ecb7be9fff17fe210fdc9377f9eb
max_bezs <52088363+maxbezs@users.noreply.github.com>
2023-07-22 02:00:42 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        domains: ["res.cloudinary.com"],
+    },
 };
 
 module.exports = nextConfig;
--- store/store.js
+++ store/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 
 export default configureStore({
-  reducer: {
-    cart: cartSlice,
-  },
+    reducer: {
+        cart: cartSlice,
+    },
 });
